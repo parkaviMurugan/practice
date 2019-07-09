220486c049f5a9c7d82d85c2b896b3fd56a0d9f4
parkaviMurugan <51223390+parkaviMurugan@users.noreply.github.com>
2019-07-09 22:00:08 +0530

    Update pro_s6_52.py

--- pro_s6_52.py
+++ pro_s6_52.py
@@ -2,7 +2,7 @@ x,y=input().split()
 a,b=input().split()
 i,j=input().split()
 m,n=input().split()
-if (x==i or x==m)and(y==j or y==n):
+if (x==i or x==m or x==a)and(y==j or y==n or y==b):
   print("yes")
 else:
   print("no")

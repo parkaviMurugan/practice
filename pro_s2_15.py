81ffad3d5be5487619f99f424ab45b25b1a41221
parkaviMurugan <51223390+parkaviMurugan@users.noreply.github.com>
2019-07-13 21:09:52 +0530

    Update pro_s2_15.py

--- pro_s2_15.py
+++ pro_s2_15.py
@@ -2,5 +2,15 @@ i=int(input())
 l=input().split()
 l=[int(i) for i in l]
 l.sort(reverse=True)
+k=[]
+f=[]
 for i in l:
+  s=str(bin(i))
+  k.append(s.count('1'))
+for i in range(len(k)):
+  m=max(k)
+  p=k.index(m)
+  f.append(l[p])
+  k[p]=0
+for i in f:
   print(i)

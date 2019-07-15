f8b4bfe85bcb9dfb233f994dab5906e084db044c
parkaviMurugan <51223390+parkaviMurugan@users.noreply.github.com>
2019-07-15 23:35:41 +1400

    Update pro_s2_15.py

--- pro_s2_15.py
+++ pro_s2_15.py
@@ -1,10 +1,10 @@
-i=int(input())
-l=input().split()
-l=[int(i) for i in l]
-l.sort(reverse=True)
+inp=int(input())
+li=input().split()
+li=[int(i) for i in li]
+li.sort(reverse=True)
 k=[]
 f=[]
-for i in l:
+for i in li:
   s=str(bin(i))
   k.append(s.count('1'))
 for i in range(len(k)):
